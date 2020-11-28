c20af153963123bb63c8f151af1f616db2c6d9ac
Steve Calverley <steve.calverley@heffron.com.au>
2020-11-29 07:48:38 +1100

    Add blog list

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,17 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        netlifycms-dev
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="mx-auto container">
+    <header>Blog Index</header>
+    <section>
+      <ul>
+      <li v-for="post of posts" :key="post.slug">
+        <NuxtLink :to="{ name: 'slug', params: { slug: post.slug } }">
+          <div>
+            <h2>{{ post.title }}</h2>
+          </div>
+        </NuxtLink>
+      </li>
+    </ul>
+    </section>
   </div>
 </template>
 
@@ -34,51 +22,18 @@ export default {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     };
   },
+  async asyncData({ $content, params }) {
+      const posts = await $content('blog', params.slug)
+        .only(['title', 'description', 'slug', 'author'])
+        .sortBy('createdAt', 'asc')
+        .fetch()
+      console.log(posts);
+      return {
+        posts
+      }
+    }
 };
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
